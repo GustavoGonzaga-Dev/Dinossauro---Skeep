c3733a70ad010312d5b4420265f34a639f7e3eff
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-03-09 15:31:37 -0300

    first commit

---
+++ src/Dinossauro.java
@@ -0,0 +1,73 @@
+import javax.swing.JOptionPane;
+
+public class Dinossauro {
+
+    private int energia;
+    private int velocidade;
+    private int temperatura;
+    private int humor;
+    public boolean vida = true;
+
+    public void pular() {
+        if (energia <= 0) {
+            JOptionPane.showMessageDialog(null, "Não posso pular agora, estou sem energia");
+        } else if (energia >= 1) {
+            energia--;
+            humor++;
+            JOptionPane.showMessageDialog(null, "Pulei");
+        }
+    }
+
+    public void correr() {
+        if (energia <= 0) {
+            JOptionPane.showMessageDialog(null, "Não posso correr agora, estou sem energia");
+        } else if (energia >= 1) {
+            energia--;
+            humor++;
+            JOptionPane.showMessageDialog(null, "Corri");
+        }
+    }
+
+    public void comer() {
+        energia++;
+        humor++;
+        JOptionPane.showMessageDialog(null, "Comi");
+    }
+
+    public void brincar() {
+        if (energia <= 0) {
+            JOptionPane.showMessageDialog(null, "Não posso brincar agora, estou sem energia");
+        } else if (energia >= 1) {
+            energia--;
+            humor++;
+            JOptionPane.showMessageDialog(null, "Brinquei");
+        }
+    }
+
+    public void tomarSol() {
+        if (energia <= 0) {
+            JOptionPane.showMessageDialog(null, "Não posso tomar sol agora, estou sem energia");
+        } else if (energia >= 1) {
+            velocidade++;
+            energia--;
+            humor++;
+            JOptionPane.showMessageDialog(null, "Tomei sol");
+        }
+    }
+
+    public void ficarNaSombra() {
+        if (humor <= 0) {
+            JOptionPane.showMessageDialog(null, "Não posso ficar na sombra agora, estou muito triste");
+        } else if (humor >= 1) {
+            energia++;
+            humor--;
+            JOptionPane.showMessageDialog(null, "Fiquei na sombra");
+        }
+    }
+
+    public void mostrarAtributos() {
+        JOptionPane.showMessageDialog(null, "ATRIBUTOS DO SKEEP: \n" + "Energia = " + energia + "\n" + "Velocidade = "
+                + velocidade + "\n" + "Temperatura = " + temperatura + "\n" + "Humor = " + humor);
+    }
+
+}
---
+++ src/Teste.java
@@ -0,0 +1,43 @@
+
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        Dinossauro Skeep = new Dinossauro();
+        // Scanner scan = new Scanner(System.in);
+        do {
+            Skeep.mostrarAtributos();
+            String textoMaiusculo = JOptionPane
+                    .showInputDialog("ATIVIDADES COM O SKEEP: \n" + "(P)ular" + "\n" + "co(R)rer" + "\n" + "(C)omer"
+                            + "\n" + "(B)rincar" + "\n" + "Tomar (S)ol" + "\n" + "Ficar na S(O)mbra");
+            textoMaiusculo = textoMaiusculo.toUpperCase();
+            // String textoMaiusculo = scan.nextLine().toUpperCase();
+            char letra = textoMaiusculo.charAt(0);
+            switch (letra) {
+            case 'P':
+                Skeep.pular();
+                break;
+            case 'R':
+                Skeep.correr();
+                break;
+            case 'C':
+                Skeep.comer();
+                break;
+            case 'B':
+                Skeep.brincar();
+                break;
+            case 'S':
+                Skeep.tomarSol();
+                break;
+            case 'O':
+                Skeep.ficarNaSombra();
+                break;
+
+            default:
+                break;
+            }
+        } while (Skeep.vida == true);
+        // scan.close();
+    }
+}
